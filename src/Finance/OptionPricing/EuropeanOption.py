8494f4871ee5f0921abaf2f4557c2ac62b943da7
Hephaestus144 <collins.storm@gmail.com>
2019-06-17 17:35:00 +0200

    TEST: Added test for European Option Monte Carlo

--- src/Finance/OptionPricing/EuropeanOption.py
+++ src/Finance/OptionPricing/EuropeanOption.py
@@ -1,7 +1,8 @@
-from StochasticProcesses.GBM import GBM
 import numpy as np
 from scipy.stats import norm
+
 from OptionPricing.OptionStyle import OptionStyle
+from StochasticProcesses.GBM import GBM
 
 
 # assume a call for now
@@ -15,17 +16,19 @@ class EuropeanOption:
         self.option_style = option_style
 
     def black_scholes_price(self):
-        d1 = (np.log(self.S0/self.K) + (self.mu + 0.5*self.sigma**2)*self.T)\
-             /(self.sigma*np.sqrt(self.T))
-        d2 = d1 - self.sigma*np.sqrt(self.T)
+        d1 = (np.log(self.S0 / self.K) + (self.mu + 0.5 * self.sigma ** 2) * self.T) \
+             / (self.sigma * np.sqrt(self.T))
+        d2 = d1 - self.sigma * np.sqrt(self.T)
         if self.option_style is OptionStyle.CALL:
-            return self.S0*norm.cdf(d1) - self.K*np.exp(-self.mu*self.T)*norm.cdf(d2)
+            return self.S0 * norm.cdf(d1) - self.K * np.exp(-self.mu * self.T) * norm.cdf(d2)
         else:
             return -self.S0 * norm.cdf(-d1) + self.K * np.exp(-self.mu * self.T) * norm.cdf(-d2)
 
     def monte_carlo_price(self, simulation_count, time_step_size):
         gbm = GBM(self.S0, self.mu, self.sigma, time_step_size, self.T, simulation_count)
         gbm.generate_paths()
-        return np.maximum(np.mean(gbm.paths) - self.K, 0)
-
-
+        prices = np.exp(-self.mu * self.T) \
+               * np.maximum(gbm.paths[:, -1] - self.K, 0)
+        price = np.mean(prices)
+        stddev = np.std(prices)/np.sqrt(simulation_count)
+        return [price, stddev]
--- src/Finance/OptionPricing/Tests/TestsEuropeanOption.py
+++ src/Finance/OptionPricing/Tests/TestsEuropeanOption.py
@@ -1,9 +1,20 @@
-from OptionPricing.EuropeanOption import EuropeanOption
+import numpy as np
 import unittest
+
+from OptionPricing.EuropeanOption import EuropeanOption
+from OptionPricing.OptionStyle import OptionStyle
 
 
 class TestsEuropeanOption(unittest.TestCase):
-    def test_basic_black_scholes(self):
-        option = EuropeanOption(50, 52, 0.1, 0.4, 5/12)
-        print(f"European option price {option.black_scholes_price()}")
+    def test_basic_call_black_scholes(self):
+        option = EuropeanOption(50, 52, 0.1, 0.4, 5/12, OptionStyle.CALL)
         self.assertAlmostEqual(option.black_scholes_price(), 5.1911, 4)
+
+    def test_basic_put_black_scholes(self):
+        option = EuropeanOption(50, 52, 0.1, 0.4, 5/12, OptionStyle.PUT)
+        self.assertAlmostEqual(option.black_scholes_price(), 5.0689, 4)
+
+    def test_monte_carlo_call_pricing(self):
+        option = EuropeanOption(50, 52, 0.1, 0.4, 5/12, OptionStyle.CALL)
+        [price, stddev] = option.monte_carlo_price(1000, 5/12)
+        self.assertTrue(5.1911 - 2*stddev < price < 5.1911 + 2*stddev)
