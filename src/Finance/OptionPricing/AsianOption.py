34f31e27cbf55d49bc55620189c12e2de742574d
Hephaestus144 <collins.storm@gmail.com>
2019-06-17 19:15:10 +0200

    FEAT: Added option_style to Asian option

--- src/Finance/OptionPricing/AsianOption.py
+++ src/Finance/OptionPricing/AsianOption.py
@@ -1,16 +1,17 @@
 import numpy as np
 from StochasticProcesses.GBM import GBM
-
+from OptionPricing.OptionStyle import OptionStyle
 
 class AsianOption:
     # assuming averaging period lasts till maturity
-    def __init__(self, initial_value, strike, drift, volatility, T, averaging_period_start):
+    def __init__(self, initial_value, strike, drift, volatility, T, averaging_period_start, option_style):
         self.S0 = initial_value
         self.K = strike
         self.mu = drift
         self.sigma = volatility
         self.T = T
         self.T_avg_start = averaging_period_start
+        self.option_style = option_style
 
     def monte_carlo_price(self, simulation_count, time_step_size):
         gbm = GBM(self.S0, self.mu, self.sigma, time_step_size, self.T, simulation_count)
@@ -18,7 +19,10 @@ class AsianOption:
         time_steps = gbm.get_time_steps()
         T_avg_start_index = np.where(time_steps >= self.T_avg_start)[0][0]
         prices = np.mean(gbm.paths[:, T_avg_start_index:], 1)
-        prices = np.exp(-self.mu*self.T)*np.maximum(prices - self.K, 0)
+        if self.option_style is OptionStyle.CALL:
+            prices = np.exp(-self.mu*self.T)*np.maximum(prices - self.K, 0)
+        else:
+            prices = np.exp(-self.mu * self.T) * np.maximum(self.K - prices, 0)
         price = np.mean(prices)
         stddev = np.std(prices) / np.sqrt(simulation_count)
         return [price, stddev]
--- src/Finance/OptionPricing/Tests/TestsAsianOption.py
+++ src/Finance/OptionPricing/Tests/TestsAsianOption.py
@@ -13,4 +13,4 @@ class TestsAsianOption(unittest.TestCase):
     def test_monte_carlo_call_pricing(self):
         option = AsianOption(50, 52, 0.1, 0.4, 5/12, 0.2)
         [price, stddev] = option.monte_carlo_price(1000, 5/(10*12))
-        self.assertTrue(5.1911 - 2*stddev < price < 5.1911 + 2*stddev)
+        self.assertTrue(3.9605 - 2*stddev < price < 3.9605 + 2*stddev)
