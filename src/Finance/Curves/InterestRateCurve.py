c4e44697d6a2300348d042df98f48722f280804b
Storm Collins <stcollins@deloitte.co.za>
2019-07-12 20:20:32 +0200

    TEST: Added test for interest rate curve discount factor extrapolation

--- src/Finance/Curves/InterestRateCurve.py
+++ src/Finance/Curves/InterestRateCurve.py
@@ -6,6 +6,9 @@ class InterestRateCurve:
     def __init__(self, tenors, discount_factors):
         self.tenors = tenors
         self.discount_factors = discount_factors
+        if not tenors.__contains__(0):
+            self.tenors = np.append(0, self.tenors)
+            self.discount_factors = np.append(1, self.discount_factors)
 
     def get_discount_factors(self, tenors):
         return np.interp(tenors, self.tenors, self.discount_factors)
--- src/Finance/Curves/Tests/TestsInterestRateCurves.py
+++ src/Finance/Curves/Tests/TestsInterestRateCurves.py
@@ -10,6 +10,13 @@ class TestsInterestRateCurves(unittest.TestCase):
     def test_discount_factor_interpolation(self):
         interest_rate_curve = InterestRateCurve(np.array([0.25, 0.5, 0.75, 1.0]), np.array([0.9, 0.85, 0.8, 0.75]))
         actual_discount_factors\
-            = interest_rate_curve.get_discount_factors(np.array([0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0]))
-        expected_discount_factors = np.array([0.9, 0.875, 0.85, 0.825, 0.8, 0.775, 0.75])
+            = interest_rate_curve.get_discount_factors(np.array([0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0]))
+        expected_discount_factors = np.array([0.95, 0.9, 0.875, 0.85, 0.825, 0.8, 0.775, 0.75])
+        np.testing.assert_array_almost_equal(expected_discount_factors, actual_discount_factors, 2)
+
+    def test_discount_factor_extrapolation(self):
+        interest_rate_curve = InterestRateCurve(np.array([0.25, 0.5, 0.75, 1.0]), np.array([0.9, 0.85, 0.8, 0.75]))
+        actual_discount_factors\
+            = interest_rate_curve.get_discount_factors(np.array([2.0]))
+        expected_discount_factors = np.array([0.75])
         np.testing.assert_array_almost_equal(expected_discount_factors, actual_discount_factors, 2)
