d904830850b7d30d1fad273b4f0a5c74497b9824
Hephaestus144 <collins.storm@gmail.com>
2019-07-12 18:04:08 +0200

    TEST: Added extrapolation test for survival probabilities

--- src/Finance/Curves/Tests/TestsSurvivalCurve.py
+++ src/Finance/Curves/Tests/TestsSurvivalCurve.py
@@ -7,12 +7,20 @@ class SurvivalCurve_Tests(unittest.TestCase):
     # driftless test
     # pfe type test
     # stats tests
-    def test_get_default_probability(self):
+    def test_get_default_probability_interpolation(self):
         tenors = np.array([0, 0.25, 0.5])
         default_probabilities = np.array([0, 0.1, 0.2])
         survival_curve = SurvivalCurve(tenors, default_probabilities, 0.4)
         expected_survival_probabilities = np.array([0, 0.05, 0.1, 0.15, 0.2])
-        print(survival_curve.get_probabilities_of_default([0, 0.125, 0.25, 0.375, 0.5]))
         np.testing.assert_array_almost_equal(
             survival_curve.get_probabilities_of_default([0, 0.125, 0.25, 0.375, 0.5]),
+            expected_survival_probabilities, 2)
+
+    def test_get_default_probability_extrapolation(self):
+        tenors = np.array([0, 0.25, 0.5])
+        default_probabilities = np.array([0, 0.1, 0.2])
+        survival_curve = SurvivalCurve(tenors, default_probabilities, 0.4)
+        expected_survival_probabilities = np.array([0.2])
+        np.testing.assert_array_almost_equal(
+            survival_curve.get_probabilities_of_default([0.75]),
             expected_survival_probabilities, 2)
