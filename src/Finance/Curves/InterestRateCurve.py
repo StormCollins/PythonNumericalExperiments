bba1b9e1c8ebbbac20327a1769616f63662754b8
Storm Collins <stcollins@deloitte.co.za>
2019-07-12 20:39:25 +0200

    FEAT: Added rate calculations for interest rate curve

--- src/Finance/Curves/InterestRateCurve.py
+++ src/Finance/Curves/InterestRateCurve.py
@@ -12,3 +12,26 @@ class InterestRateCurve:
 
     def get_discount_factors(self, tenors):
         return np.interp(tenors, self.tenors, self.discount_factors)
+
+    def get_zero_rates(self, tenors, rate_convention):
+        discount_factors = self.get_discount_factors(tenors)
+        if rate_convention == RateConvention.NACC:
+            return -np.log(discount_factors) / tenors
+        elif rate_convention == RateConvention.NACS:
+            # df = (1 + r/2)^(2t)
+            # r = 2*[df^(-1/2t) - 1]
+            return 2*(discount_factors**(-1/(2*tenors)) - 1)
+        elif rate_convention == RateConvention.NACM:
+            # df = (1 + r/2)^(2t)
+            # r = 2*[df^(-1/2t) - 1]
+            return 12*(discount_factors**(-1/(12*tenors)) - 1)
+        elif rate_convention == RateConvention.NACA:
+            # df = (1 + r/2)^(2t)
+            # r = 2*[df^(-1/2t) - 1]
+            return discount_factors**(-1/tenors) - 1
+        elif rate_convention == RateConvention.NACQ:
+            # df = (1 + r/2)^(2t)
+            # r = 2*[df^(-1/2t) - 1]
+            return 4*(discount_factors**(-1/(4*tenors)) - 1)
+        else:
+            return "Invalid rate convention"
