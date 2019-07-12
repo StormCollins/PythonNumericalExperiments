e837594ff26fcd938666ddc6352d92a5c8027cbc
Storm Collins <stcollins@deloitte.co.za>
2019-07-12 20:59:22 +0200

    FEAT: Added functionality to calculate CVA

--- src/Finance/XVA/CVA.py
+++ src/Finance/XVA/CVA.py
@@ -1,10 +1,14 @@
 from src.Finance.Curves.SurvivalCurve import SurvivalCurve
+from src.Finance.Curves.InterestRateCurve import InterestRateCurve
 
 import numpy as np
 
 class CVA:
-    def __init__(self, survival_curve):
+    def __init__(self, interest_rate_curve: InterestRateCurve, survival_curve: SurvivalCurve):
+        self.interest_rate_curve = interest_rate_curve
         self.survival_curve = survival_curve
 
-    def compute_cva(self, price_paths):
-        np.maximum(price_paths, 0)
+    def compute_cva(self, tenors, price_paths):
+        discount_factors = self.interest_rate_curve.get_zero_rates(tenors)
+        default_probabilities = self.survival_curve.get_probabilities_of_default(tenors)
+        return np.average(np.sum((1 - self.survival_curve.recovery_rate) * np.maximum(price_paths, 0)*discount_factors*default_probabilities, 1))
