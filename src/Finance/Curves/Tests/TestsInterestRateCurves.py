6cccd3a9e79f3289449829754eb2baff955eee63
Storm Collins <stcollins@deloitte.co.za>
2019-07-12 20:44:42 +0200

    TEST: Added test for zero rate calculations for interest rate curves

--- src/Finance/Curves/Tests/TestsInterestRateCurves.py
+++ src/Finance/Curves/Tests/TestsInterestRateCurves.py
@@ -20,3 +20,11 @@ class TestsInterestRateCurves(unittest.TestCase):
             = interest_rate_curve.get_discount_factors(np.array([2.0]))
         expected_discount_factors = np.array([0.75])
         np.testing.assert_array_almost_equal(expected_discount_factors, actual_discount_factors, 2)
+
+    def test_zero_rates_without_interpolation(self):
+        tenors = np.array([0.25, 0.5, 0.75, 1.0])
+        expected_rates = np.array([0.1, 0.11, 0.12, 0.13])
+        discount_factors = np.exp(-tenors * expected_rates)
+        interest_rate_curve = InterestRateCurve(tenors, discount_factors)
+        actual_rates = interest_rate_curve.get_zero_rates(tenors, RateConvention.NACC)
+        np.testing.assert_array_almost_equal(expected_rates, actual_rates, 2)
